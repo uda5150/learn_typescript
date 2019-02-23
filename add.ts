797883aeb11c3878829d36937a0190bc30844fe9
uda5150 <tks.udagawa@gmail.com>
2019-02-24 01:48:39 +0900

    修正

--- add.ts
+++ add.ts
@@ -10,6 +10,12 @@ const decimalToBinary = (n: number) => {
   return Number(binaryN.join(''));
 }
 
+// console.log("decimalToBinary(0) =", decimalToBinary(0));
+// console.log("decimalToBinary(1) =", decimalToBinary(1));
+// console.log("decimalToBinary(2) =", decimalToBinary(2));
+// console.log("decimalToBinary(40) =", decimalToBinary(40));
+// console.log("decimalToBinary(100) =", decimalToBinary(100));
+
 // 2進数を10進数に変換
 const binaryToDecimal = (n: number) => {
   const digits = n.toString().length;
@@ -23,6 +29,12 @@ const binaryToDecimal = (n: number) => {
   
   return decimalN;
 }
+
+// console.log("binaryToDecimal(0) =", binaryToDecimal(0));
+// console.log("binaryToDecimal(1) =", binaryToDecimal(1));
+// console.log("binaryToDecimal(10) =", binaryToDecimal(10));
+// console.log("binaryToDecimal(101000) =", binaryToDecimal(101000));
+// console.log("binaryToDecimal(1100100) =", binaryToDecimal(1100100));
 
 // 2進数の足し算
 const addBinary = (n: number, m: number) => {
@@ -38,10 +50,13 @@ const addBinary = (n: number, m: number) => {
 
   for (let i = digits - 1; i >= 0; i--) {
     if (i === 0){
-      tmpResultList.unshift(1);
+      if (tmpResultList[i] >= 2) {
+        tmpResultList[i] = tmpResultList[i] % 2;
+        tmpResultList.unshift(1);
+      }
     } else {
       if (tmpResultList[i] >= 2) {
-        tmpResultList[i - 1] =+ 1;
+        tmpResultList[i - 1] += 1;
         tmpResultList[i] = tmpResultList[i] % 2;
       };
     };
@@ -50,11 +65,26 @@ const addBinary = (n: number, m: number) => {
   return Number(tmpResultList.join(''));
 }
 
+// console.log("addBinary(0, 0) =", addBinary(0, 0));
+// console.log("addBinary(0, 1) =", addBinary(0, 1));
+// console.log("addBinary(1, 0) =", addBinary(1, 0));
+// console.log("addBinary(1, 1) =", addBinary(1, 1));
+// console.log("addBinary(10101, 10011) =", addBinary(10101, 10011));
+// console.log("addBinary(11011010, 10110110) =", addBinary(11011010, 10110110));
+
 // 10進数の足し算
 const addDecimal = (n: number, m: number) => {
   const resultBinary = addBinary(decimalToBinary(n), decimalToBinary(m));
-  
+
   return binaryToDecimal(resultBinary);
 }
 
-console.log(addDecimal(12, 12));
+
+console.log("0 + 0 =", addDecimal(0, 0));
+console.log("0 + 1 =", addDecimal(0, 1));
+console.log("1 + 1 =", addDecimal(1, 1));
+console.log("12 + 12 =", addDecimal(12, 12));
+console.log("21 + 19 =", addDecimal(21, 19));
+console.log("299 + 1 =", addDecimal(299, 1));
+console.log("7 + 7 =", addDecimal(7, 7));
+console.log("218 + 182 =", addDecimal(218, 182)); //11011010 10110110
